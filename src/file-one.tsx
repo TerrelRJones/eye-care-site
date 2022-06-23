b7cf6569237eb0cc45aa66eee05059be0274f0ad
Terrel Jones <terrel.jones@bounteous.com>
2022-06-23 11:42:25 -0700

    moved files to components directory and renamed files

--- src/file-one.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function file-one() {
-  return (
-    <div>file-one</div>
-  )
-}
-
-export default file-one
--- src/file-two.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const fileTwo = (props: Props) => {
-  return <div>Words</div>;
-};
