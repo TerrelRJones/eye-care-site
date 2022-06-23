cc6c64d2372dbe2899e25515e5358ea27080d2ec
Terrel Jones <terrel.jones@bounteous.com>
2022-06-23 11:45:33 -0700

    moved Hero component into Hero directory, and added Hero.scss file

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {};
+
+const Hero = (props: Props) => {
+  return (
+    <div>
+      <div>
+        <h1>This is a hero image</h1>
+        <p>And this is some text</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
