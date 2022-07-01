0c364ed60d106ef845e7139386c24cfa20400254
Terrel Jones <terrel.jones@bounteous.com>
2022-07-01 09:35:24 -0700

    created stories for card container

---
+++ src/components/CardContainer/CardContainer.stories.jsx
@@ -0,0 +1,15 @@
+import CardContainer from '.';
+
+
+export default {
+  title: 'Containers',
+  component: CardContainer,
+
+} 
+
+const Template= (args) => <CardContainer {...args} />
+
+export const Primary = Template.bind({});
+Primary.args = {
+    title: "VSP Benefits are easy to use"
+}
