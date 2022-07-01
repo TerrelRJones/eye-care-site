02112c78346441c64a7793a36144aedf296da9b2
Terrel Jones <terrel.jones@bounteous.com>
2022-07-01 09:58:16 -0700

    All changes made.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Colors } from "../../../src/styles/colors";
+import { Colors } from "styles/colors";
 
 export type ButtonProps = {
   label: string;
@@ -30,24 +30,24 @@ const ButtonStyled = styled.button<
 
   ${({ buttonType, disabled }) => {
     // Primary Btn
-    if (buttonType === "primary" && !disabled) {
-      return `color: ${Colors.white}; background-color: ${Colors.mountainBlue}};  &:hover {background: ${Colors.icyBlue}; border: 2px solid ${Colors.icyBlue}}`;
-    } else if (buttonType === "primary" && disabled) {
-      return `color: ${Colors.white}; background-color: ${Colors.matteGray}; border: 2px solid ${Colors.matteGray}`;
+    if (buttonType === "primary") {
+      return disabled
+        ? `color: ${Colors.white}; background-color: ${Colors.matteGray}; border: 2px solid ${Colors.matteGray}`
+        : `color: ${Colors.white}; background-color: ${Colors.mountainBlue}};  &:hover {background: ${Colors.icyBlue}; border: 2px solid ${Colors.icyBlue}}`;
     }
 
     // Secondary Btn
-    if (buttonType === "secondary" && !disabled) {
-      return `color: ${Colors.mountainBlue}; background-color: transparent; &:hover {color: ${Colors.icyBlue}; border: 2px solid ${Colors.icyBlue}};`;
-    } else if (buttonType === "secondary" && disabled) {
-      return `color: ${Colors.white}; background-color: ${Colors.matteGray}; border: 2px solid ${Colors.matteGray}`;
+    if (buttonType === "secondary") {
+      return disabled
+        ? `color: ${Colors.white}; background-color: ${Colors.matteGray}; border: 2px solid ${Colors.matteGray}`
+        : `color: ${Colors.mountainBlue}; background-color: transparent; &:hover {color: ${Colors.icyBlue}; border: 2px solid ${Colors.icyBlue}};`;
     }
 
     // Tertiary Btn
-    if (buttonType === "tertiary" && !disabled) {
-      return `color: ${Colors.mountainBlue}; background-color: ${Colors.lime}; border: 2px solid ${Colors.lime}; &:hover {color: ${Colors.white}; background-color: ${Colors.icyBlue}; border: 2px solid ${Colors.icyBlue}};`;
-    } else if (buttonType === "tertiary" && disabled) {
-      return `color: ${Colors.white}; background-color: ${Colors.matteGray}; border: 2px solid ${Colors.matteGray}`;
+    if (buttonType === "tertiary") {
+      return disabled
+        ? `color: ${Colors.white}; background-color: ${Colors.matteGray}; border: 2px solid ${Colors.matteGray}`
+        : `color: ${Colors.mountainBlue}; background-color: ${Colors.lime}; border: 2px solid ${Colors.lime}; &:hover {color: ${Colors.white}; background-color: ${Colors.icyBlue}; border: 2px solid ${Colors.icyBlue}};`;
     }
   }};
 
@@ -69,7 +69,7 @@ export const Button = ({
   return (
     <ButtonStyled buttonType={buttonType} disabled={disabled} onClick={onClick}>
       {label}
-      <div className="icon">{icon}</div>
+      {icon && <div className="icon">{icon}</div>}
     </ButtonStyled>
   );
 };
