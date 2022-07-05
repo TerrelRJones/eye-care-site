7f60993cc398736dc016dab083db2e725661b444
Terrel Jones <terrel.jones@bounteous.com>
2022-07-05 12:05:45 -0700

    Built out footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,50 +1,40 @@
 import Styled from "styled-components";
 import { Colors } from "styles/colors";
 
-import VSPLogo from "assets/images/vsp_logo_secondary.png";
-
-type Props = {};
+import { ReactComponent as VSPLogo } from "assets/images/vsp_logo_secondary.svg";
+import { FooterLinks } from "const/FooterLinks";
 
 const StyledFooter = Styled.footer`
     background-color: ${Colors.mountainBlue};
-    width: 100vw;
-    /* height: 458px; */
+    width: 100%;
 `;
 
 const FooterContainer = Styled.div`
-display: flex;
-flex-direction: column;
-justify-content: space-between;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     max-width: 1200px;
     height: 100%;
     margin: 0 auto;
-    /* background-color: red; */
-    
+    padding: 64px 0;
 `;
 
 const FooterCardContainer = Styled.div`
     display: flex;
     justify-content: space-between;
-    margin-top: 50px;
-    margin-bottom: 50px;
-    /* display: grid;
-    place-items: center;
-    grid-template-columns: repeat(5, 1fr) ; */
+    margin-bottom: 40px;
 
     @media(max-width: 576px){
-        flex-direction: column;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr) ;
+      padding: 10px;
+
     }
 
 `;
 
 const FooterCard = Styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    /* background: green; */
-    padding: 10px;
-
+      padding: 10px;
     .title {
         color: ${Colors.white};
         font-family: "Open Sans";
@@ -61,18 +51,9 @@ const FooterCard = Styled.div`
     }
     `;
 
-const LogoContainer = Styled.div`
-    display: flex;
-    align-items: baseline;
-  color: ${Colors.white};
-  font-family: "Open Sans";
-  font-size: 12px;
-  letter-spacing: 0;
-  line-height: 20px;
-    
-`;
-
 const Link = Styled.a`
+        display: flex;
+        align-items: center;
         text-decoration: none;
         color: ${Colors.white};
         font-family: "Open Sans";
@@ -82,72 +63,63 @@ const Link = Styled.a`
         line-height: 20px;
         margin-bottom: 8px;
         text-transform: capitalize;
+
+        img {
+        width: 1rem;
+        margin-right: 5px;
+      }
+
 `;
 
-export const Footer = (props: Props) => {
+const LogoContainer = Styled.div`
+    display: flex;
+    align-items: baseline;
+    color: ${Colors.white};
+    font-family: "Open Sans";
+    font-size: 12px;
+    letter-spacing: 0;
+    line-height: 20px;
+
+    .logo {
+      margin-right: 10px;
+    }
+
+    p {
+      margin-right: 5px;
+    }
+
+  @media(max-width: 576px){
+    flex-direction: column;
+    align-items: center;
+  }
+    
+`;
+
+export const Footer = () => {
   return (
     <StyledFooter>
       <FooterContainer>
         <FooterCardContainer>
-          <FooterCard>
-            <div className="title">About</div>
-            <div className="links">
-              <Link href="/">About This Site</Link>
-              <Link href="/">About VSP</Link>
-              <Link href="/">Become A VSP Provider</Link>
-              <Link href="/">Careers</Link>
-              <Link href="/">Frame Gallery</Link>
-              <Link href="/">Site Map</Link>
-            </div>
-          </FooterCard>
-          <FooterCard>
-            <div className="title">Contact</div>
-            <div className="links">
-              <Link href="/">About This Site</Link>
-              <Link href="/">About VSP</Link>
-              <Link href="/">Become A VSP Provider</Link>
-              <Link href="/">Careers</Link>
-              <Link href="/">Frame Gallery</Link>
-              <Link href="/">Site Map</Link>
-            </div>
-          </FooterCard>
-          <FooterCard>
-            <div className="title">Legal and Privacy</div>
-            <div className="links">
-              <Link href="/">About This Site</Link>
-              <Link href="/">Non-Discrimination Statement</Link>
-              <Link href="/">Become A VSP Provider</Link>
-              <Link href="/">Careers</Link>
-              <Link href="/">Frame Gallery</Link>
-              <Link href="/">Site Map</Link>
-            </div>
-          </FooterCard>
-          <FooterCard>
-            <div className="title">About</div>
-            <div className="links">
-              <Link href="/">About This Site</Link>
-              <Link href="/">About VSP</Link>
-              <Link href="/">Become A VSP Provider</Link>
-              <Link href="/">Careers</Link>
-              <Link href="/">Frame Gallery</Link>
-              <Link href="/">Site Map</Link>
-            </div>
-          </FooterCard>
-          <FooterCard>
-            <div className="title">About</div>
-            <div className="links">
-              <Link href="/">About This Site</Link>
-              <Link href="/">About VSP</Link>
-              <Link href="/">Become A VSP Provider</Link>
-              <Link href="/">Careers</Link>
-              <Link href="/">Frame Gallery</Link>
-              <Link href="/">Site Map</Link>
-            </div>
-          </FooterCard>
+          {FooterLinks.map(({ title, links }: any) => (
+            <FooterCard>
+              <div className="title">{title}</div>
+              {links.map(({ text, url, image, alt }: any) => (
+                <div className="links">
+                  <Link href={url} target="_blank">
+                    {image ? <img src={image} alt={alt} /> : null}
+                    {text}
+                  </Link>
+                </div>
+              ))}
+            </FooterCard>
+          ))}
         </FooterCardContainer>
         <LogoContainer>
-          <img src={VSPLogo} alt="VSP Logo" />
-          <p>© 2018 Vision Service Plan. All rights reserved.</p>
+          <div className="logo">
+            <VSPLogo />
+          </div>
+          <p>© 2018 Vision Service Plan.</p>
+          <p>All rights reserved.</p>
         </LogoContainer>
       </FooterContainer>
     </StyledFooter>
