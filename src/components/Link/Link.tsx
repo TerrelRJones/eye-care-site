735033cc357a71a4c31210f667d6d40cacbb9a6c
Terrel Jones <terrel.jones@bounteous.com>
2022-07-05 16:44:04 -0700

    refactored link component to take args

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -3,16 +3,18 @@ import { Colors } from "styles/colors";
 
 type LinkProps = {
   url: string;
-  alt: string;
+  alt?: string;
   text: string;
   image?: string;
+  color?: string;
+  hoverColor?: string;
 };
 
-const LinkStyled = Styled.a`
+const LinkStyled = Styled.a<Pick<LinkProps, "color" | "hoverColor">>`
         display: flex;
         align-items: center;
         text-decoration: none;
-        color: ${Colors.white};
+        color: ${({ color }) => (color ? color : Colors.white)};
         font-family: "Open Sans";
         font-size: 12px;
         font-weight: bold;
@@ -20,6 +22,12 @@ const LinkStyled = Styled.a`
         line-height: 20px;
         margin-bottom: 8px;
         text-transform: capitalize;
+        transition: all .2s ease-in-out;
+
+        ${({ hoverColor }) =>
+          hoverColor
+            ? ` &:hover { color: ${hoverColor}; text-decoration: underline;}`
+            : null}
 
         img {
         width: 1rem;
@@ -28,9 +36,21 @@ const LinkStyled = Styled.a`
 
 `;
 
-export const Link = ({ url, alt, text, image }: LinkProps) => {
+export const Link = ({
+  url,
+  alt,
+  text,
+  image,
+  color,
+  hoverColor,
+}: LinkProps) => {
   return (
-    <LinkStyled href={url} target="_blank">
+    <LinkStyled
+      href={url}
+      color={color}
+      hoverColor={hoverColor}
+      target="_blank"
+    >
       {image ? <img src={image} alt={alt} /> : null}
       {text}
     </LinkStyled>
