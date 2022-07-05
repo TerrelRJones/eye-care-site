b17d191d287c5b895cb337a10eb2528630d05ba6
Terrel Jones <terrel.jones@bounteous.com>
2022-07-05 16:43:25 -0700

    add bg img, and turned links into data

---
+++ src/const/ContactUsLinks.js
@@ -0,0 +1,14 @@
+export const ContactUsLinks = [
+  {
+    title: "Call Us",
+    url: "/",
+  },
+  {
+    title: "Email Member Services",
+    url: "/",
+  },
+  {
+    title: "File a Grievance",
+    url: "/",
+  },
+];
