0296be782204477888267dd0aeec273103c88f26
Terrel Jones <terrel.jones@bounteous.com>
2022-07-06 08:04:48 -0700

    changed file extension to .ts

---
+++ src/const/ContactUsLinks.ts
@@ -0,0 +1,15 @@
+export const ContactUsLinks = [
+  {
+    title: "Call Us",
+    url: "/",
+  },
+  {
+    title: "Email Member Services",
+    url: "/",
+  },
+  {
+    title: "File a Grievance",
+    url: "/",
+  },
+];
+
