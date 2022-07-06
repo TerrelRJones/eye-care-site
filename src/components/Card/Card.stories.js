a671e6d5ad7f4d268f37b20aef98f9d8f8d73722
Terrel Jones <terrel.jones@bounteous.com>
2022-07-06 13:40:09 -0700

    removed white space

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -1,24 +1,19 @@
-
-import { Card } from './Card';
-import { HeartIcon } from 'assets/icons/Icons';
+import { Card } from "./Card";
+import { HeartIcon } from "assets/icons/Icons";
 
 export default {
-  title: 'Card',
+  title: "Card",
   component: Card,
+};
 
-} 
-
-const Template= (args) => <Card {...args} />
+const Template = (args) => <Card {...args} />;
 
 export const CTACard = Template.bind({});
 CTACard.args = {
-  title: 'Review your benefits',
+  title: "Review your benefits",
   text: "Review your benefits with VSP network doctors",
   icon: <HeartIcon />,
   buttonType: "primary",
   buttonLabel: "View Benefits",
   disabled: false,
-  
-
-
 };
