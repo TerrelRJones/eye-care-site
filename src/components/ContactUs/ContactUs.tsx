f83a800e0a56eb559fe02f1b02c1a1afd00a8176
Terrel Jones <terrel.jones@bounteous.com>
2022-07-11 11:14:45 -0700

    changed Styled to styled

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -1,4 +1,4 @@
-import Styled from "styled-components";
+import styled from "styled-components";
 import { Colors } from "styles/colors";
 
 import BG_IMAGE from "assets/images/background_image.jpeg";
@@ -10,130 +10,127 @@ type ContactUsProps = {
   linksTitle: string;
 };
 
-const ContactUsStyled = Styled.div`
-    position: relative;
+const ContactUsStyled = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 40px;
+  background-color: ${Colors.mountainBlue};
+
+  .sections {
+    position: absolute;
     display: flex;
+    align-items: center;
     justify-content: center;
-    align-items:center;
-    width: 100%;
-    margin-bottom: 40px;
-    background-color: ${Colors.mountainBlue};
+    /* flex-wrap: wrap; */
+    gap: 30px;
+  }
+
+  @media (max-width: 850px) {
+    flex-direction: column;
+    background-color: ${Colors.white};
+    margin-bottom: 0;
+    gap: 0;
 
     .sections {
-        position: absolute;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        /* flex-wrap: wrap; */
-        gap: 30px;
-
+      flex-direction: column;
+      position: static;
     }
-
-    @media(max-width: 850px){
-        flex-direction: column;
-        background-color: ${Colors.white};
-        margin-bottom: 0;
-        gap: 0;
-
-
-        .sections {
-            flex-direction: column;
-            position: static;
-        }
-    }
+  }
 `;
 
-const ContactUsContainer = Styled.div`
-   
-    width: 1200px;
-    height: 350px;
-    background-image: url(${BG_IMAGE});
-    background-repeat: no-repeat;
-    background-position: center; 
-    background-size: cover;
+const ContactUsContainer = styled.div`
+  width: 1200px;
+  height: 350px;
+  background-image: url(${BG_IMAGE});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
 
-    @media(max-width: 850px){
-        width: 100%;
-    }
+  @media (max-width: 850px) {
+    width: 100%;
+  }
 `;
 
-const LeftSection = Styled.section`
+const LeftSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 669px;
+  min-height: 208px;
+  background-color: ${Colors.whiteOpacity};
+
+  .test {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    max-width: 669px;
-    min-height: 208px;
-    background-color: ${Colors.whiteOpacity};
+    width: 80%;
+  }
 
-    .test {
-        display: flex;
-        width: 80%;
-    }
-
-    @media(max-width: 850px){
-        width: 100%;
-    }
+  @media (max-width: 850px) {
+    width: 100%;
+  }
 `;
 
-const RightSection = Styled.section`
+const RightSection = styled.section`
+  display: flex;
+  justify-content: center;
+  min-width: 270px;
+  min-height: 208px;
+  background-color: ${Colors.whiteOpacity};
+
+  @media (max-width: 850px) {
+    width: 100%;
+  }
+`;
+
+const RightContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+
+  .title {
+    color: ${Colors.mountainBlue};
+    font-family: "Open Sans";
+    font-size: 14px;
+    letter-spacing: 0;
+    line-height: 40px;
+    margin-bottom: 20px;
+  }
+
+  .links {
     display: flex;
-    justify-content: center;
-    min-width: 270px;
-    min-height: 208px;
-    background-color: ${Colors.whiteOpacity};
-
-    @media(max-width: 850px){
-        width: 100%;
-    }
+    flex-direction: column;
+    justify-content: space-between;
+    height: 60%;
+  }
 `;
 
-const RightContent = Styled.div`
-        display: flex;
-        flex-direction: column;
-        width: 80%;
-
-    .title {
-        color: ${Colors.mountainBlue}
-        font-family: "Open Sans";
-        font-size: 14px;
-        letter-spacing: 0;
-        line-height: 40px;
-        margin-bottom: 20px;
-    }
-
-    .links {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        height: 60%;
-    }
+const VerticalDesignElement = styled.div`
+  box-sizing: border-box;
+  height: 67px;
+  width: 3px;
+  border: 4px solid ${Colors.lime};
+  margin-right: 20px;
 `;
 
-const VerticalDesignElement = Styled.div`
-    box-sizing: border-box;
-    height: 67px;
-    width: 3px;
-    border: 4px solid ${Colors.lime};
-    margin-right: 20px;
-`;
-
-const Text = Styled.div`
-    h1{
+const Text = styled.div`
+  h1 {
     color: ${Colors.mountainBlue};
     font-family: Oswald;
     font-size: 38px;
     letter-spacing: 0;
     line-height: 41.8px;
     margin: 0;
-    }
+  }
 
-    p{
+  p {
     color: ${Colors.mountainBlue};
     font-family: "Open Sans";
     font-size: 16px;
     letter-spacing: 0;
-    line-height: 25px; 
-    }
+    line-height: 25px;
+  }
 `;
 
 export const ContactUs = ({ title, linksTitle }: ContactUsProps) => {
