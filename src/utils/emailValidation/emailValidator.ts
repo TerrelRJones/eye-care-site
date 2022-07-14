5ee8fae9782d137fd2ead7f5a05603aec38965ed
Terrel Jones <terrel.jones@bounteous.com>
2022-07-14 16:15:07 -0700

    clean up

--- src/utils/emailValidation/emailValidator.ts
+++ src/utils/emailValidation/emailValidator.ts
@@ -3,17 +3,17 @@ export const emailValidation = (email: string) => {
   const MAX_RECIPIENT_LENGTH = 64;
   const recipient = checkEmail.split("@")[0].split("");
   const lastCharOfRecipient = recipient.length - 1;
-
   // clean up tomorrow 7/15
   const emailCheck = new RegExp(/[\w!@#$%^&*()-+/?.]+@[\w]+\.(com|edu|org)/);
   const isValidEmail = emailCheck.test(checkEmail);
 
   if (recipient[0].includes(".")) return "Email can not start with a '.'";
+
   if (recipient[lastCharOfRecipient].includes("."))
     return "Email can not end with a '.'";
 
   if (recipient.length >= MAX_RECIPIENT_LENGTH)
-    return `Email characters exceeded limit of ${MAX_RECIPIENT_LENGTH}.`;
+    return `Email characters exceeded limit of ${MAX_RECIPIENT_LENGTH} characters.`;
 
   // checking if ".." exist witin recipient
   for (let i = 1; i < recipient.length - 2; i++) {
