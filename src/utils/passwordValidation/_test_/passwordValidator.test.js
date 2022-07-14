6083b526210414090841bff6fa4fc56dfd6a18fc
Terrel Jones <terrel.jones@bounteous.com>
2022-07-14 10:40:12 -0700

    fixed some edgecases and test

--- src/utils/passwordValidation/_test_/passwordValidator.test.js
+++ src/utils/passwordValidation/_test_/passwordValidator.test.js
@@ -2,32 +2,26 @@ import { passwordValidation } from "../passwordValidator";
 
 describe("TEST for password function", () => {
   it('Should return "Password can not be password."', () => {
-    const pass = passwordValidation("password", "Terrel");
+    const pass = passwordValidation("password", "tjones");
 
     expect(pass).toBe("Password can not be password.");
   });
 
-  // it('Should return "Password must contain a upercase character."', () => {
-  //   const pass = passwordValidation("jonessss", "Terrel");
-
-  //   expect(pass).toBe("Password must contain an upercase character.");
-  // });
-
-  it("Should return if password is valid true", () => {
-    const pass = passwordValidation("Jonestr125bl!", "Terrel");
-
-    expect(pass).toBe(true);
-  });
-
-  it("Should return if", () => {
-    const pass = passwordValidation("Jonestr125bl!", "jOnes");
+  it("Should return 'Password can not contain username or name.'", () => {
+    const pass = passwordValidation("HeyJones3488!", "tjones");
 
     expect(pass).toBe("Password can not contain username or name.");
   });
 
-  it("Should return 'Must contain a upercase character.'", () => {
-    const pass = passwordValidation("terrelrj3!ones", "Chris");
+  it("Should return true", () => {
+    const pass = passwordValidation("terrelrj3!ones", "tjones");
 
-    expect(pass[0]).toBe("Password must contain an uppercase character.");
+    expect(pass).toBe(true);
+  });
+
+  it("Should return 'Password must contain atleast 3 rules.'", () => {
+    const pass = passwordValidation("terrelrddsdsdsdes", "tjones");
+
+    expect(pass).toBe("Password must contain atleast 3 rules.");
   });
 });
--- src/utils/passwordValidation/passwordValidator.ts
+++ src/utils/passwordValidation/passwordValidator.ts
@@ -2,6 +2,9 @@ import { passwordValidationChecks } from "./passValidationData";
 
 export const passwordValidation = (password: string, username: string) => {
   const checkPass = password.toString();
+
+  const checkUsername = username.substring(1);
+
   const errors: Array<{}> = [];
 
   if (checkPass === "") return "Please enter a password.";
@@ -9,7 +12,7 @@ export const passwordValidation = (password: string, username: string) => {
   if (checkPass.toLowerCase().includes("password"))
     return "Password can not be password.";
 
-  if (checkPass.toLowerCase().includes(username.toLowerCase()))
+  if (checkPass.toLowerCase().includes(checkUsername.toLowerCase()))
     return "Password can not contain username or name.";
 
   passwordValidationChecks.forEach(({ regEx, message }) => {
@@ -18,7 +21,8 @@ export const passwordValidation = (password: string, username: string) => {
     }
   });
 
-  if (errors.length === 0) return true;
+  if (errors.length <= 2) return true;
+  if (errors.length >= 3) return "Password must contain atleast 3 rules.";
 
   return errors;
 };
