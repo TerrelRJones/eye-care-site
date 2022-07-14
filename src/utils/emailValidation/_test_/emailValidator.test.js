a337cde44ed32ae00b897f1f0927c79f2aa4c53a
Terrel Jones <terrel.jones@bounteous.com>
2022-07-14 16:00:08 -0700

    finished email validation func; wrote test

--- src/utils/emailValidation/_test_/emailValidator.test.js
+++ src/utils/emailValidation/_test_/emailValidator.test.js
@@ -1,11 +1,39 @@
 import { emailValidation } from "../emailValidator";
 
 describe("Testing email validation function", () => {
-  it("Should pass true", () => {
+  it("Should pass true if email is valid.", () => {
     expect(emailValidation("TJones@gmail.com")).toBe(true);
   });
 
-  it("Should fail for . in beggining.", () => {
-    expect(emailValidation(".TJones@gmail.com")).toBe(false);
+  it("Should fail for . in beggining of email.", () => {
+    expect(emailValidation(".TJones@gmail.com")).toBe(
+      "Email can not start with a '.'"
+    );
+  });
+
+  it("Should fail for . being at the end of email recipient.", () => {
+    expect(emailValidation("TJones.@gmail.com")).toBe(
+      "Email can not end with a '.'"
+    );
+  });
+
+  it("Should fail for '..' being inside recipient.", () => {
+    expect(emailValidation("TJ..nes@gmail.com")).toBe(false);
+  });
+
+  it("Should fail for characters being over 64.", () => {
+    expect(
+      emailValidation(
+        "terreljon1terreljon1terreljon1terreljon1terreljon1terreljon12345@gmail.com"
+      )
+    ).toBe("Email characters exceeded limit of 64.");
+  });
+
+  it("Should pass for characters being 63 long.", () => {
+    expect(
+      emailValidation(
+        "terreljon1terreljon1terreljon1terreljon1terreljon1terreljon1234@gmail.com"
+      )
+    ).toBe(true);
   });
 });
--- src/utils/emailValidation/emailValidator.ts
+++ src/utils/emailValidation/emailValidator.ts
@@ -1,7 +1,25 @@
 export const emailValidation = (email: string) => {
-  const emailCheck = new RegExp(
-    /[^.][\w!@#$%^&*()-+/?.]+@[\w]+\.(com|edu|org)/
-  );
+  const checkEmail = email.toString();
+  const MAX_RECIPIENT_LENGTH = 64;
+  const recipient = checkEmail.split("@")[0].split("");
+  const lastCharOfRecipient = recipient.length - 1;
 
-  return emailCheck.test(email);
+  // clean up tomorrow 7/15
+  const emailCheck = new RegExp(/[\w!@#$%^&*()-+/?.]+@[\w]+\.(com|edu|org)/);
+  const isValidEmail = emailCheck.test(checkEmail);
+
+  if (recipient[0].includes(".")) return "Email can not start with a '.'";
+  if (recipient[lastCharOfRecipient].includes("."))
+    return "Email can not end with a '.'";
+
+  if (recipient.length >= MAX_RECIPIENT_LENGTH)
+    return `Email characters exceeded limit of ${MAX_RECIPIENT_LENGTH}.`;
+
+  // checking if ".." exist witin recipient
+  for (let i = 1; i < recipient.length - 2; i++) {
+    const forbidden = "..";
+    let adjacentVals = recipient[i] + recipient[i + 1];
+    if (adjacentVals === forbidden) return false;
+  }
+  return isValidEmail;
 };
