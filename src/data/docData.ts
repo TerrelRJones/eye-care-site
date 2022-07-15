c520f8a17507ad46de542297c8624e3330e9446b
Terrel Jones <terrel.jones@bounteous.com>
2022-07-15 11:05:10 -0700

    added return values

--- src/data/docData.ts
+++ src/data/docData.ts
@@ -1,4 +1,4 @@
-interface DoctorData {
+export interface DoctorData {
   id: number;
   first_name: string;
   last_name: string;
--- src/utils/doctorFunctions.ts
+++ src/utils/doctorFunctions.ts
@@ -1,4 +1,4 @@
-import docData from "../../src/data/docData";
+import docData, { DoctorData } from "../../src/data/docData";
 
 export enum Network {
   ADVANTAGE = "Advantage",
@@ -10,24 +10,30 @@ export enum JobTitle {
   OPTOMETRIST = "Optometrist",
 }
 
-export const filterByNetwork = (network: Network) => {
+export const filterByNetwork = (network: Network): DoctorData[] => {
   const newDocs = docData.filter(({ doctor_networks }) =>
     doctor_networks?.includes(network)
   );
   console.log(newDocs);
+  return newDocs;
 };
 
-export const filterByJobTitle = (title: JobTitle) => {
+export const filterByJobTitle = (title: JobTitle): DoctorData[] => {
   const newDocs = docData.filter(({ job_title }) => job_title === title);
   console.log(newDocs);
+  return newDocs;
 };
 
-export const searchByName = (name: string) => {
+export const searchByName = (name: string): DoctorData[] => {
   const newDocs = docData.filter(({ first_name }) =>
     first_name.toLowerCase().includes(name.toLocaleLowerCase())
   );
 
-  if (newDocs.length === 0) return console.log("No Docs By That Name");
+  if (newDocs.length === 0) {
+    console.log("No Docs By That Name");
+    return newDocs;
+  }
 
   console.log(newDocs);
+  return newDocs;
 };
