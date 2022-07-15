d7b68b543f38cc3a5f5d94f5dc568d2327b6e2b7
Terrel Jones <terrel.jones@bounteous.com>
2022-07-15 11:15:20 -0700

    updated function return types. and test

--- src/utils/passwordValidation/_test_/passwordValidator.test.js
+++ src/utils/passwordValidation/_test_/passwordValidator.test.js
@@ -4,19 +4,21 @@ describe("TEST for password function", () => {
   it('Should return "Password can not be password."', () => {
     const pass = passwordValidation("password", "tjones");
 
-    expect(pass).toBe("Password can not be password.");
+    expect(pass.errors[0]).toBe("Password can not be password.");
   });
 
   it("Should return 'Password can not contain username or name.'", () => {
     const pass = passwordValidation("HeyJones3488!", "tjones");
 
-    expect(pass).toBe("Password can not contain username or name.");
+    expect(pass.errors[0]).toBe("Password can not contain username or name.");
   });
 
   it("Should return 'Username invalid; must be atleast 2 characters.' If user pass in 1 character.", () => {
     const pass = passwordValidation("HeyJones3488!", "t");
 
-    expect(pass).toBe("Username invalid; must be atleast 2 characters.");
+    expect(pass.errors[0]).toBe(
+      "Username invalid; must be atleast 2 characters."
+    );
   });
 
   it("Should return valid if 3 rules are met", () => {
