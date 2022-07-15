51e80e2822cf589510eb576c20cabe32792a3340
Terrel Jones <terrel.jones@bounteous.com>
2022-07-15 11:59:20 -0700

    updated return response and test

--- src/utils/phoneNumberValidation/phoneNumberValidation.ts
+++ src/utils/phoneNumberValidation/phoneNumberValidation.ts
@@ -1,10 +1,18 @@
-export const phoneNumberValidator = (phoneNumber: string) => {
-  const checkNumber = phoneNumber.toString().split("");
+interface PhoneNumberValidatorResponse {
+  isValid: boolean;
+  phoneNumber?: string;
+  error: string | null;
+}
+
+export const phoneNumberValidator = (
+  phoneNumber: string
+): PhoneNumberValidatorResponse => {
+  const checkNumber = phoneNumber.split("");
   let formattedNumber = "";
 
   checkNumber.forEach((number) => {
     if (number === "0" || number === "1")
-      return "Please enter a valid phone number.";
+      return { isValid: false, error: "Please enter a valid phone number." };
 
     if (Number(number)) {
       formattedNumber = formattedNumber += number;
@@ -12,14 +20,16 @@ export const phoneNumberValidator = (phoneNumber: string) => {
   });
 
   if (formattedNumber.length < 10 || formattedNumber.length > 10)
-    return "Please enter a valid phone number.";
+    return { isValid: false, error: "Please enter a valid phone number." };
 
   if (!formattedNumber[3].match(/[2-9]/))
-    return "Please enter a valid phone number.";
+    return { isValid: false, error: "Please enter a valid phone number." };
 
   let match = formattedNumber.match(/^(\d{3})(\d{3})(\d{4})$/);
 
-  if (match) return `(${match[1]}) ${match[2]}-${match[3]}`;
+  if (match) {
+    formattedNumber = `(${match[1]}) ${match[2]}-${match[3]}`;
+  }
 
-  return formattedNumber;
+  return { isValid: true, phoneNumber: formattedNumber, error: null };
 };
