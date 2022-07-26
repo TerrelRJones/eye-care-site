b70f7e3a753cd5d8097d81a68023b5dd6a616141
Terrel Jones <terrel.jones@bounteous.com>
2022-07-26 14:44:19 -0700

    added stories for diff doc cards

--- src/components/DoctorInfoCard/DoctorInfoCard.stories.jsx
+++ src/components/DoctorInfoCard/DoctorInfoCard.stories.jsx
@@ -1,4 +1,5 @@
 import { DoctorInfoCard } from "./DoctorInfoCard";
+import Docs from "data/docData";
 
 export default {
   title: "Doctor Info Card",
@@ -7,5 +8,22 @@ export default {
 
 const Template = (args) => <DoctorInfoCard {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {};
+export const Female = Template.bind({});
+Female.args = {
+  doctor: Docs[6],
+};
+
+export const Male = Template.bind({});
+Male.args = {
+  doctor: Docs[2],
+};
+
+export const NoNetwork = Template.bind({});
+NoNetwork.args = {
+  doctor: Docs[1],
+};
+
+export const NoAvi = Template.bind({});
+NoAvi.args = {
+  doctor: Docs[4],
+};
